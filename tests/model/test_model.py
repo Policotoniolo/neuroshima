164254f0bcebda1af7973cd2765ef089d86ee023
Policotoniolo <paul.toniolo@wanadoo.fr>
2024-12-17 17:29:19 +0100

    implement test Hand and Player

--- tests/model/test_model.py
+++ tests/model/test_model.py
@@ -34,7 +34,7 @@ class TestTile():
         print(f"Tearing down {method}\n")
         del self.tile
 
-    ### Test rotations ###
+    #### tests
     
     def test__direction_positive_rotation(self):
         result = self.tile._direction_positive_rotation((0,-1,1))
@@ -68,7 +68,6 @@ class TestTile():
                             {"cac_augment":[(-1, 0, 1),(0, -1, +1), (1, -1, 0)]},
                             {"range_augment":[(-1, 0, 1),(0, -1, +1), (1, -1, 0)]}
         ]
-
 
     def test_rotation_wrong(self):
         with pytest.raises(ValueError):
@@ -146,6 +145,8 @@ class TestDeck:
         print(f"Tearing down {method}\n")
         del self.deck
 
+    #### tests
+
     def test_deck_initialization(self):
         assert len(self.deck.tiles) == 2
         assert self.deck.hq_tile.kind == "base"
@@ -169,7 +170,6 @@ class TestDeck:
         assert set(original_order) == set(shuffled_order)
         assert len(original_order) == len(shuffled_order)
 
-
     def test_dict_to_tile_wrong(self):
         wrong_dic_type = ["some wrong data"]
         with pytest.raises(TypeError):
@@ -184,7 +184,6 @@ class TestDeck:
 
         mock_import_module.assert_called_once_with("scripts.armies.TestArmy")
         assert result == self.mock_get_army
-
 
     @mock.patch("importlib.import_module", side_effect=ModuleNotFoundError)
     def test_get_army_module_not_found(self, mock_import_module):
@@ -206,4 +205,149 @@ class TestDeck:
 ### Tests Deck Class ###
 
 class TestHand():
-    pass
+    @mock.patch('scripts.model.model.Tile')
+    def setup_method(self, method, mocktile):
+        print(f"Setting up {method}")
+
+        self.hand = model.Hand()
+
+        mocktile.id_tile.return_value = 'test_tile'
+        self.mocktile = mocktile
+        self.hand.hand_tiles.append(mocktile)
+
+    def teardown_method(self, method):
+        print(f"Tearing down {method}\n")
+        del self.hand
+        del self.mocktile
+
+    #### tests
+
+    @mock.patch('scripts.model.model.Tile')
+    def test_add_tile_success(self, newmocktile):
+        newmocktile.id_tile.return_value = 'new_test_tile'
+        self.hand.add_tile(newmocktile)
+        assert len(self.hand.hand_tiles) == 2
+        assert newmocktile in self.hand.hand_tiles
+        assert self.mocktile in self.hand.hand_tiles
+
+    @mock.patch('scripts.model.model.Tile')
+    def test_add_tile_wrong(self, newmocktile):
+        self.hand.add_tile(newmocktile)
+        self.hand.add_tile(newmocktile)
+        with pytest.raises(ValueError): 
+            self.hand.add_tile(newmocktile)
+
+    def test_discard_tile_success(self):
+        self.hand.discard_tile([self.mocktile])
+        assert len(self.hand.hand_tiles) == 0
+
+    @mock.patch('scripts.model.model.Tile')
+    def test_discard_tile_wrong(self, mockwrongtile):
+        mockwrongtile.id_tile.return_value = "wrong_tile"
+        with pytest.raises(ValueError):
+            self.hand.discard_tile([mockwrongtile])
+
+    def test_get_tile_by_id_success(self):
+        id_tile = self.mocktile.id_tile
+        result = self.hand.get_tile_by_id(id_tile)
+        assert result == self.mocktile
+
+    def test_get_tile_by_id_wrong(self):
+        with pytest.raises(ValueError):
+            self.hand.get_tile_by_id(id_tile="wrong_id")
+
+
+### Tests Player Class ###
+
+class TestPlayer():
+    def mock_get_army(self):
+        return [{"kind": "base",
+        "army_name":"armytest",
+        "id_tile":"armytest-qg",
+        "initiative": [0],
+        "range_attacks_direction":None,
+        "range_attacks_power":None,
+        "cac_attacks_direction":[(0, -1, +1), 
+                                (1, -1, 0),
+                                (1, 0, -1),
+                                (0, 1, -1),
+                                (-1, 1, 0),
+                                (-1, 0, 1)
+                                ],
+        "cac_attacks_power":[1,1,1,1,1,1],
+        "net_directions":None,
+        "life_point":20,
+        "shields_directions":None,
+        "special_capacities":[],
+        "module":None,
+        "action":None,
+        "url_image":"image/armies/armytest-qg.png"},
+    {"kind": "unite",
+        "army_name":"armytest",
+        "id_tile":"armytest1",
+        "initiative": [2],
+        "range_attacks_direction":None,
+        "range_attacks_power":None,
+        "cac_attacks_direction":[(-1, 0, 1),(0, -1, +1), (1, -1, 0)],
+        "cac_attacks_power":[1,1,1],
+        "net_directions":None,
+        "life_point":1,
+        "shields_directions":None,
+        "special_capacities":[],
+        "module":None,
+        "action":None,
+        "url_image":"image/armies/armytest-mutant.png"},
+    {"kind": "unite",
+        "army_name":"armytest",
+        "id_tile":"armytest2",
+        "initiative": [2],
+        "range_attacks_direction":None,
+        "range_attacks_power":None,
+        "cac_attacks_direction":[(-1, 0, 1),(0, -1, +1), (1, -1, 0)],
+        "cac_attacks_power":[1,1,1],
+        "net_directions":None,
+        "life_point":1,
+        "shields_directions":None,
+        "special_capacities":[],
+        "module":None,
+        "action":None,
+        "url_image":"image/armies/armytest-mutant.png"}
+        ]
+        
+    def setup_method(self, method):
+        print(f"Setting up {method}")
+        with pytest.MonkeyPatch.context() as m:
+            m.setattr(model.Deck, "_get_army", self.mock_get_army)
+            self.player = model.Player("testname", "testarmy")
+
+    def teardown_method(self, method):
+        print(f"Tearing down {method}\n")
+        del self.player
+
+
+    def test_get_tiles_success(self):
+        tile_to_draw = self.player.deck.tiles[0]
+        self.player.get_tiles(1)
+
+        assert tile_to_draw in self.player.hand.hand_tiles
+        assert tile_to_draw not in self.player.deck.tiles
+        assert len(self.player.hand.hand_tiles) == 1
+
+    def test_get_tiles_wrong_value(self):
+        with pytest.raises(ValueError):
+            self.player.get_tiles(4)
+
+    def test_get_tiles_zero_ask(self):
+        old_hand = self.player.hand.hand_tiles.copy()
+        old_deck = self.player.deck.tiles.copy()
+        self.player.get_tiles(0)
+        assert self.player.hand.hand_tiles == old_hand
+        assert self.player.deck.tiles == old_deck
+
+    def test_discard_tiles_hand_success(self):
+        pass
+
+    def test_discard_tiles_hand_empty_hand(self):
+        self.player.hand.hand_tiles = []
+        result = self.player.discard_tiles_hand()
+        assert result == None
