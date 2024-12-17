33d645b0ecc932fef1d7bc1a307a1f0c1314a62e
Policotoniolo <paul.toniolo@wanadoo.fr>
2024-12-17 13:30:18 +0100

    Implement Deck Class unittests

--- tests/model/test_model.py
+++ tests/model/test_model.py
@@ -1,4 +1,5 @@
 import pytest
+import unittest.mock as mock
 
 import scripts.model.model as model
 import scripts.utils.config as config
@@ -80,63 +81,65 @@ class TestTile():
 ### Tests Deck Class ###
 
 class TestDeck:
+
+    def mock_get_army(self):
+        return [{"kind": "base",
+        "army_name":"armytest",
+        "id_tile":"armytest-qg",
+        "initiative": [0],
+        "range_attacks_direction":None,
+        "range_attacks_power":None,
+        "cac_attacks_direction":[(0, -1, +1), 
+                                (1, -1, 0),
+                                (1, 0, -1),
+                                (0, 1, -1),
+                                (-1, 1, 0),
+                                (-1, 0, 1)
+                                ],
+        "cac_attacks_power":[1,1,1,1,1,1],
+        "net_directions":None,
+        "life_point":20,
+        "shields_directions":None,
+        "special_capacities":[],
+        "module":None,
+        "action":None,
+        "url_image":"image/armies/armytest-qg.png"},
+    {"kind": "unite",
+        "army_name":"armytest",
+        "id_tile":"armytest1",
+        "initiative": [2],
+        "range_attacks_direction":None,
+        "range_attacks_power":None,
+        "cac_attacks_direction":[(-1, 0, 1),(0, -1, +1), (1, -1, 0)],
+        "cac_attacks_power":[1,1,1],
+        "net_directions":None,
+        "life_point":1,
+        "shields_directions":None,
+        "special_capacities":[],
+        "module":None,
+        "action":None,
+        "url_image":"image/armies/armytest-mutant.png"},
+    {"kind": "unite",
+        "army_name":"armytest",
+        "id_tile":"armytest2",
+        "initiative": [2],
+        "range_attacks_direction":None,
+        "range_attacks_power":None,
+        "cac_attacks_direction":[(-1, 0, 1),(0, -1, +1), (1, -1, 0)],
+        "cac_attacks_power":[1,1,1],
+        "net_directions":None,
+        "life_point":1,
+        "shields_directions":None,
+        "special_capacities":[],
+        "module":None,
+        "action":None,
+        "url_image":"image/armies/armytest-mutant.png"}
+        ]
+
     def setup_method(self, method):
         print(f"Setting up {method}")
-        def mock_get_army(self):
-            return [{"kind": "base",
-            "army_name":"borgo",
-            "id_tile":"borgo-qg",
-            "initiative": [0],
-            "range_attacks_direction":None,
-            "range_attacks_power":None,
-            "cac_attacks_direction":[(0, -1, +1), 
-                                    (1, -1, 0),
-                                    (1, 0, -1),
-                                    (0, 1, -1),
-                                    (-1, 1, 0),
-                                    (-1, 0, 1)
-                                    ],
-            "cac_attacks_power":[1,1,1,1,1,1],
-            "net_directions":None,
-            "life_point":20,
-            "shields_directions":None,
-            "special_capacities":[],
-            "module":None,
-            "action":None,
-            "url_image":"image/armies/borgo-qg.png"},
-        {"kind": "unite",
-            "army_name":"borgo",
-            "id_tile":"borgo-mutant1",
-            "initiative": [2],
-            "range_attacks_direction":None,
-            "range_attacks_power":None,
-            "cac_attacks_direction":[(-1, 0, 1),(0, -1, +1), (1, -1, 0)],
-            "cac_attacks_power":[1,1,1],
-            "net_directions":None,
-            "life_point":1,
-            "shields_directions":None,
-            "special_capacities":[],
-            "module":None,
-            "action":None,
-            "url_image":"image/armies/borgo-mutant.png"},
-        {"kind": "unite",
-            "army_name":"borgo",
-            "id_tile":"borgo-mutant2",
-            "initiative": [2],
-            "range_attacks_direction":None,
-            "range_attacks_power":None,
-            "cac_attacks_direction":[(-1, 0, 1),(0, -1, +1), (1, -1, 0)],
-            "cac_attacks_power":[1,1,1],
-            "net_directions":None,
-            "life_point":1,
-            "shields_directions":None,
-            "special_capacities":[],
-            "module":None,
-            "action":None,
-            "url_image":"image/armies/borgo-mutant.png"}
-            ]
         with pytest.MonkeyPatch.context() as m:
-            m.setattr(model.Deck, "_get_army", mock_get_army)
+            m.setattr(model.Deck, "_get_army", self.mock_get_army)
             self.deck = model.Deck("TestArmy")
 
     def teardown_method(self, method):
@@ -155,8 +158,8 @@ class TestDeck:
     def test_deck_remove_top_tile(self):
         initial_size = len(self.deck.tiles)
         top_tile = self.deck.remove_top_deck_tile()
-        assert (top_tile.id_tile == "borgo-mutant1" or
-                top_tile.id_tile == "borgo-mutant2")
+        assert (top_tile.id_tile == "armytest1" or
+                top_tile.id_tile == "armytest2")
         assert len(self.deck.tiles) == initial_size - 1
 
     def test_deck_shuffle(self):
@@ -166,3 +169,41 @@ class TestDeck:
         assert set(original_order) == set(shuffled_order)
         assert len(original_order) == len(shuffled_order)
 
+
+    def test_dict_to_tile_wrong(self):
+        wrong_dic_type = ["some wrong data"]
+        with pytest.raises(TypeError):
+            self.deck._dict_to_tile(wrong_dic_type)
+
+    @mock.patch("importlib.import_module")
+    def test_get_army_success(self, mock_import_module):
+        mock_module = mock.MagicMock()
+        setattr(mock_module, "TestArmy", self.mock_get_army)
+        mock_import_module.return_value = mock_module
+        result = self.deck._get_army()
+
+        mock_import_module.assert_called_once_with("scripts.armies.TestArmy")
+        assert result == self.mock_get_army
+
+
+    @mock.patch("importlib.import_module", side_effect=ModuleNotFoundError)
+    def test_get_army_module_not_found(self, mock_import_module):
+        with pytest.raises(ValueError, match="Army module 'TestArmy' not found."):
+            self.deck._get_army()
+        mock_import_module.assert_called_once_with("scripts.armies.TestArmy")
+
+    @mock.patch("importlib.import_module")
+    def test_get_army_attribute_not_found(self, mock_import_module):
+        mock_module = mock.MagicMock()
+        del mock_module.TestArmy
+        mock_import_module.return_value = mock_module
+
+        with pytest.raises(ValueError, match="Army 'TestArmy' not defined in the module."):
+            self.deck._get_army()
+        mock_import_module.assert_called_once_with("scripts.armies.TestArmy")
+
+
+### Tests Deck Class ###
+
+class TestHand():
+    pass
